1e794c823a610d98c44e331e42fb0df94959eb5b
Mario RC <marioromerocardona@gmail.com>
2015-09-21 13:13:14 +0200

    directorio relativo output

--- compilarDTOdebug.bat
+++ compilarDTOdebug.bat
@@ -1,7 +1,7 @@
 rem PATH de MSBuild para compilar el proyecto
 SET MSBuild="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 rem PATH para la salida de la compilacion
-SET OUTPUT_PATH=".\Output"
+SET OUTPUT_PATH="..\..\Output"
 rem PATH para RSVARS -necesario-
 SET RSVARS="C:\Program Files (x86)\Embarcadero\Studio\14.0\bin\rsvars.bat"
 rem llamada a rsvars.bat
