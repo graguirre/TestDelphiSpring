e4e734b7c97530732e33b2a172c02e33e2bc82a8
Mario RC <marioromerocardona@gmail.com>
2015-09-21 12:47:56 +0200

    Añadido .bat

---
+++ compilarDTOdebug.bat
@@ -0,0 +1,11 @@
+<NotepadPlus>
+    <Session activeView="0">
+        <mainView activeIndex="1">
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="7208" startPos="929" endPos="929" selMode="0" lang="Batch" encoding="-1" filename="C:\Users\marioromero\Documents\JENKINS\ProyectoPrueba\Vistas\compilarRelease.bat" backupFilePath="" originalFileLastModifTimestamp="1442500889" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="7208" startPos="537" endPos="537" selMode="0" lang="Batch" encoding="-1" filename="C:\Users\marioromero\Documents\JENKINS\ProyectoPrueba\Librerias\compilarDebug.bat" backupFilePath="" originalFileLastModifTimestamp="1442832314" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="7208" startPos="843" endPos="843" selMode="0" lang="Batch" encoding="-1" filename="C:\Desarrollo\Test\Unitarios\Clientes\compilarJenkinsGrupoClientes.bat" backupFilePath="" originalFileLastModifTimestamp="1442562913" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="7208" startPos="1103" endPos="1103" selMode="0" lang="Batch" encoding="-1" filename="C:\Users\marioromero\Documents\GitHub\delphi-jenkins-private\TestsUnitarios\ejecutarTestsUnitarios.bat" backupFilePath="" originalFileLastModifTimestamp="1442827877" />
+        </mainView>
+        <subView activeIndex="0" />
+    </Session>
+</NotepadPlus>
