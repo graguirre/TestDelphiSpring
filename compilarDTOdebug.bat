e40925dc4f3445dcabfd3fc7fa32b86c323afcd7
Mario RC <marioromerocardona@gmail.com>
2015-09-21 12:52:38 +0200

    Cambios en el .bat

--- compilarDTOdebug.bat
+++ compilarDTOdebug.bat
@@ -1,11 +1,11 @@
-<NotepadPlus>
-    <Session activeView="0">
-        <mainView activeIndex="1">
-            <File firstVisibleLine="0" xOffset="0" scrollWidth="7208" startPos="929" endPos="929" selMode="0" lang="Batch" encoding="-1" filename="C:\Users\marioromero\Documents\JENKINS\ProyectoPrueba\Vistas\compilarRelease.bat" backupFilePath="" originalFileLastModifTimestamp="1442500889" />
-            <File firstVisibleLine="0" xOffset="0" scrollWidth="7208" startPos="537" endPos="537" selMode="0" lang="Batch" encoding="-1" filename="C:\Users\marioromero\Documents\JENKINS\ProyectoPrueba\Librerias\compilarDebug.bat" backupFilePath="" originalFileLastModifTimestamp="1442832314" />
-            <File firstVisibleLine="0" xOffset="0" scrollWidth="7208" startPos="843" endPos="843" selMode="0" lang="Batch" encoding="-1" filename="C:\Desarrollo\Test\Unitarios\Clientes\compilarJenkinsGrupoClientes.bat" backupFilePath="" originalFileLastModifTimestamp="1442562913" />
-            <File firstVisibleLine="0" xOffset="0" scrollWidth="7208" startPos="1103" endPos="1103" selMode="0" lang="Batch" encoding="-1" filename="C:\Users\marioromero\Documents\GitHub\delphi-jenkins-private\TestsUnitarios\ejecutarTestsUnitarios.bat" backupFilePath="" originalFileLastModifTimestamp="1442827877" />
-        </mainView>
-        <subView activeIndex="0" />
-    </Session>
-</NotepadPlus>
+rem PATH de MSBuild para compilar el proyecto
+SET MSBuild="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+rem PATH para los BUILDS
+SET BUILDS=C:\Program Files (x86)\Jenkins\jobs\pruebacompilacionlibreria\builds\
+rem PATH para la salida de la compilacion
+SET OUTPUT_PATH=".\Output"
+rem PATH para RSVARS -necesario-
+SET RSVARS="C:\Program Files (x86)\Embarcadero\Studio\14.0\bin\rsvars.bat"
+rem llamada a rsvars.bat
+CALL %RSVARS%
+%MSBuild% DTO.dproj /t:Build /p:Config=Debug;Platform=Win32;DCC_DcuOutput=%OUTPUT_PATH%
