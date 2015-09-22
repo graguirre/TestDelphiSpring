d4faaac6471e7fa523442243e9d7d354d878f220
Mario RC <marioromerocardona@gmail.com>
2015-09-22 10:47:43 +0200

    Ejecutable bat para compilar y ejecutar los tests.

---
+++ UnitTests/ejecutarUnitTests.bat
@@ -0,0 +1,11 @@
+rem PATH de MSBuild para compilar el proyecto
+SET MSBuild="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+rem PATH para la salida de la compilacion
+SET OUTPUT_PATH="..\..\..\Output"
+SET SEARCH_PATH="..\..\..\Output"
+rem PATH para RSVARS -necesario-
+SET RSVARS="C:\Program Files (x86)\Embarcadero\Studio\14.0\bin\rsvars.bat"
+rem llamada a rsvars.bat
+CALL %RSVARS%
+%MSBuild% %1 /t:Build /p:Config=Debug;Platform=Win32;DCC_DcuOutput=%OUTPUT_PATH%;DCC_UnitSearchPath=%SEARCH_PATH%
+%MSBuild% %2 /t:Build /p:Config=Debug;Platform=Win32;DCC_UnitSearchPath=%SEARCH_PATH%
