2ff036bcf905447d9754118f5a259bb99061b48c
graguirre <graguirre@gmail.com>
2015-09-22 11:29:44 +0200

    test de tiempo

--- UnitTests/ModuloIOUnitTest.pas
+++ UnitTests/ModuloIOUnitTest.pas
@@ -6,6 +6,7 @@ uses
   Classes,
   DUnitX.TestFramework,
   SysUtils,
+  system.Diagnostics,
   StrUtils,
   InterfaceIO
   ;
@@ -15,7 +16,7 @@ type
   TestModuloIO = class(TObject)
     private
       FModuloIO : IModuloIO;
-
+      Stopwatch : TStopwatch;
     public
       [Setup]
         procedure Setup;
@@ -24,6 +25,10 @@ type
       [Test]
         [TestCase('caso 1', '10')]
         procedure TestLecturaEscritura(pValor : String);
+
+      [Test]
+        [TestCase('caso tiempo 1','1000')]
+        procedure TestTiempos(pValor: Integer);
 
   end;
 
@@ -38,6 +43,7 @@ uses
 procedure TestModuloIO.Setup;
 begin
   FModuloIO := TModuloIOArchivo.Create;
+  Stopwatch := TStopWatch.StartNew;
 end;
 
 procedure TestModuloIO.TearDown;
@@ -54,6 +60,17 @@ begin
   Assert.AreEqual(FModuloIO.Leer, pValor, 'Se esperaba '+pValor+', se obtuvo ' + output)
 end;
 
+
+procedure TestModuloIO.TestTiempos(pValor: Integer);
+var
+  i : Integer;
+  Elapsed : int64;
+begin
+  for i := 0 to pValor do
+    TestLecturaEscritura(i.ToString);
+  Elapsed := Stopwatch.ElapsedMilliseconds;
+  Assert.IsTrue(Elapsed < 1000, 'Tiempo excesivo ' + Elapsed.ToString + 'ms');
+end;
 
 initialization
   TDUnitX.RegisterTestFixture(TestModuloIO);
