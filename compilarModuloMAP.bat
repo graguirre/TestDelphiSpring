3d27c1b64499b4de374e944df421c18906ac54ec
Mario RC <marioromerocardona@gmail.com>
2015-09-23 12:23:26 +0200

    Añadido .bat prueba generación fichero del proyecto .map para la cobertura de código.

---
+++ compilarModuloMAP.bat
@@ -0,0 +1,10 @@
+rem PATH de MSBuild para compilar el proyecto
+SET MSBuild="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+rem PATH para la salida de la compilacion
+SET OUTPUT_PATH="..\..\Output"
+SET OUTPUT_MAP_PATH="..\..\Coverage"
+rem PATH para RSVARS -necesario-
+SET RSVARS="C:\Program Files (x86)\Embarcadero\Studio\14.0\bin\rsvars.bat"
+rem llamada a rsvars.bat
+CALL %RSVARS%
+%MSBuild% %1 /t:Build /p:Config=Debug;Platform=Win32;DCC_DcuOutput=%OUTPUT_PATH%;BaseOutputPath=%OUTPUT_MAP_PATH%
