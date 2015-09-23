d2b7678d73cd1b814510144e5aa47df6771b9797
Mario RC <marioromerocardona@gmail.com>
2015-09-23 13:27:07 +0200

    Cambios en bats para la cobertura de codigo

--- UnitTests/ejecutarUnitTests.bat
+++ UnitTests/ejecutarUnitTests.bat
@@ -3,6 +3,7 @@ SET MSBuild="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 rem PATH para la salida de la compilacion
 SET OUTPUT_PATH="..\..\..\Output"
 SET SEARCH_PATH="..\..\..\Output"
+SET OUTPUT_COVERAGE_PATH="..\..\CoverageReports"
 rem PATH para RSVARS -necesario-
 SET RSVARS="C:\Program Files (x86)\Embarcadero\Studio\14.0\bin\rsvars.bat"
 rem llamada a rsvars.bat
@@ -10,4 +11,6 @@ CALL %RSVARS%
 %MSBuild% %1 /t:Build /p:Config=Debug;Platform=Win32;DCC_DcuOutput=%OUTPUT_PATH%;DCC_UnitSearchPath=%SEARCH_PATH%
 %MSBuild% %2 /t:Build /p:Config=Debug;Platform=Win32;DCC_UnitSearchPath=%SEARCH_PATH%
 
-".\UnitTests\Win32\Debug\%3"
+rem ".\UnitTests\Win32\Debug\%3"
+rem ejecuta la libreria de metricas y genera los resultados
+%CodeCoverage% -e ".\UnitTests\Win32\Debug\%3" -u %1 -od %OUTPUT_COVERAGE_PATH% -html -xml -emma -lt
--- compilarModulo.bat
+++ compilarModulo.bat
@@ -2,8 +2,12 @@ rem PATH de MSBuild para compilar el proyecto
 SET MSBuild="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 rem PATH para la salida de la compilacion
 SET OUTPUT_PATH="..\..\Output"
+SET OUTPUT_BPL_PATH="..\..\Coverage"
 rem PATH para RSVARS -necesario-
 SET RSVARS="C:\Program Files (x86)\Embarcadero\Studio\14.0\bin\rsvars.bat"
 rem llamada a rsvars.bat
 CALL %RSVARS%
-%MSBuild% %1 /t:Build /p:Config=Debug;Platform=Win32;DCC_DcuOutput=%OUTPUT_PATH%
+rem Compila el proyecto y genera los archivos de la BPL en el %OUTPUT_MAP_PATH%
+%MSBuild% %1 /t:Build /p:Config=Debug;Platform=Win32;DCC_DcuOutput=%OUTPUT_PATH%;DCC_BplOutput=%OUTPUT_BPL_PATH%
+rem Mueve el fichero map del proyecto a la ruta del proyecto para la generacion de las metricas
+move %OUTPUT_MAP_PATH%\*.map %2
