10ad5d85e25c884f9597740d8e14d2dbb31906e5
graguirre <graguirre@gmail.com>
2015-09-28 10:35:54 +0200

    rectificando output path para que  funcione en jenkins

--- BPL/DTOs/compilar.bat
+++ BPL/DTOs/compilar.bat
@@ -4,6 +4,6 @@ rem PATH para RSVARS -necesario-
 CALL "C:\Program Files (x86)\Embarcadero\Studio\14.0\bin\rsvars.bat"
 rem PATH para la salida de la compilacion
 SET SEARCH_PATH="Win32\Debug"
-SET OUTPUT_PATH=".\$(Platform)\$(Config)"
+SET OUTPUT_PATH="Win32\Debug"
 
 %MSBuild% %1 /t:Build /p:Config=Debug;Platform=Win32;DCC_DcuOutput=%OUTPUT_PATH%;DCC_UnitSearchPath=%SEARCH_PATH%
