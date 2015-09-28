67b557bdd89a37b79937b6437cc326fdc77ae863
graguirre <graguirre@gmail.com>
2015-09-28 10:11:09 +0200

    compilando cada BPL por separado

---
+++ BPL/DTOs/compilar.bat
@@ -0,0 +1,9 @@
+rem PATH de MSBuild para compilar el proyecto
+SET MSBuild="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+rem PATH para la salida de la compilacion
+SET SEARCH_PATH="Win32\Debug"
+rem PATH para RSVARS -necesario-
+SET RSVARS="C:\Program Files (x86)\Embarcadero\Studio\14.0\bin\rsvars.bat"
+rem llamada a rsvars.bat
+CALL %RSVARS%
+%MSBuild% %1 /t:Build /p:Config=Debug;Platform=Win32;DCC_UnitSearchPath=%SEARCH_PATH%
