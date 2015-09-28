a2551a326c089d80adb0cbda9772966a4c6dd558
graguirre <graguirre@gmail.com>
2015-09-28 12:50:44 +0200

    probando que cada módulo tenga su archivo de compilación
    de esta manera las BPL del IDE tendrían una relación directa con las
    opciones del archivo que compila

---
+++ Test/Unitarios/ModulosBasicos/compilar.bat
@@ -0,0 +1,9 @@
+rem PATH de MSBuild para compilar el proyecto
+SET MSBuild="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+rem PATH para RSVARS -necesario-
+CALL "C:\Program Files (x86)\Embarcadero\Studio\14.0\bin\rsvars.bat"
+rem PATH para la salida de la compilacion
+SET SEARCH_PATH="Win32\Debug"
+SET OUTPUT_PATH="Win32\Debug"
+
+%MSBuild% %1 /t:Build /p:Config=Debug;Platform=Win32;DCC_DcuOutput=%OUTPUT_PATH%;DCC_UnitSearchPath=%SEARCH_PATH%
