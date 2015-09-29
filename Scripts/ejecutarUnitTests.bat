0f877ced858ef2e324ae2f382a8b249ec8bb7c34
Mario RC <marioromerocardona@gmail.com>
2015-09-29 12:32:42 +0200

    Cambios

--- Scripts/ejecutarUnitTests.bat
+++ Scripts/ejecutarUnitTests.bat
@@ -1,11 +1,13 @@
 rem PATH de MSBuild para compilar el proyecto
 SET MSBuild="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 rem PATH para la salida de la compilacion
-SET OUTPUT_PATH="..\..\Output"
-SET SEARCH_PATH="..\..\artifacts"
+SET OUTPUT_PATH="Output"
+SET SEARCH_PATH="artifacts"
 SET OUTPUT_BPL_PATH="Coverage"
 SET COVERAGE_EXE_PATH=C:\jenkins\DelphiCodeCoverage
 SET OUTPUT_COVERAGE_PATH="CoverageReports"
+SET OUTPUT_TESTS_PATH="TestsReports"
+SET OUTPUT_TESTS_FILENAME="dunitx-results.xml"
 SET OUTPUT_EMMA_PATH="EmmaReports"
 SET OUTPUT_EMMA_FILENAME="coverage.xml"
 
@@ -16,12 +18,13 @@ rem PATH para CodeCoverage
 SET CodeCoverage=%COVERAGE_EXE_PATH%\CodeCoverage.exe
 rem llamada a rsvars.bat
 CALL %RSVARS%
-%MSBuild% %1 /t:Build /p:Config=Debug;Platform=Win32;DCC_DcuOutput=%OUTPUT_PATH%;DCC_UnitSearchPath=%SEARCH_PATH%
-%MSBuild% %2 /t:Build /p:Config=Debug;Platform=Win32;DCC_ExeOutput=%OUTPUT_PATH%;DCC_BplOutput=%OUTPUT_PATH%;DCC_UnitSearchPath=%SEARCH_PATH%
+%MSBuild% %1 /t:Build /p:Config=Debug;Platform=Win32;DCC_DcuOutput=..\..\%OUTPUT_PATH%;DCC_UnitSearchPath=..\..\%SEARCH_PATH%
+%MSBuild% %2 /t:Build /p:Config=Debug;Platform=Win32;DCC_ExeOutput=..\..\%OUTPUT_PATH%;DCC_BplOutput=..\..\%OUTPUT_PATH%;DCC_UnitSearchPath=..\..\%SEARCH_PATH%
 
-rem Mueve el fichero map del proyecto a la ruta del proyecto para la generacion de las metricas
-rem move %OUTPUT_BPL_PATH%\*.map %5
-rem "%3"
+
+rem Mueve el fichero de los resultados de los tests
+move %OUTPUT_PATH%/dunitx-results.xml %OUTPUT_TESTS_PATH%\%OUTPUT_TESTS_FILENAME% 
+
 rem ejecuta la libreria de metricas y genera los resultados
 dir
 move artifacts\* Output
