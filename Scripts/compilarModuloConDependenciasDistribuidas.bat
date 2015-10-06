c7b98bcd12d3f08efe03ed446115a95ea67ba53d
Mario RC <marioromerocardona@gmail.com>
2015-10-06 11:34:47 +0200

    Nuevo .bat para compilar distribuido

---
+++ Scripts/compilarModuloConDependenciasDistribuidas.bat
@@ -0,0 +1,11 @@
+rem PATH de MSBuild para compilar el proyecto
+SET MSBuild="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+rem PATH para la salida de la compilacion
+SET OUTPUT_PATH="..\..\Output"
+SET OUTPUT_BPL_PATH="..\..\BPL"
+SET SEARCH_PATH="..\..\ThirdParty\spring4d"
+rem PATH para RSVARS -necesario-
+SET RSVARS="C:\Program Files (x86)\Embarcadero\Studio\14.0\bin\rsvars.bat"
+rem llamada a rsvars.bat
+CALL %RSVARS%
+%MSBuild% %1 /t:Build /p:Config=Debug;Platform=Win32;DCC_DcuOutput=%OUTPUT_PATH%;DCC_UnitSearchPath=%SEARCH_PATH%;DCC_BplOutput=%OUTPUT_BPL_PATH%
